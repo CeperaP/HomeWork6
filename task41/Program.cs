698de7b558987717608b05b7d9632a516c991dbd
Sergey <rogozhinis@mail.ru>
2023-01-31 16:49:48 +0500

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// Ввод чисел останавливается при помощи ввода стоп-слова "stop" и производится при помощи нажатия Enter.
+
+
+int count = 0;
+int number =0;
+string text;
+
+while (true)
+{
+    Console.WriteLine("Введите число");
+    text =Console.ReadLine(); // как избавиться от подчеркивания?
+    
+    if (text!="stop")
+    {
+        number = Convert.ToInt32 (text);    
+          if (number > 0)
+            {
+                 count++;
+            }
+       
+    }
+    else                                    
+    {                                       
+         System.Console.WriteLine($"Количество положительных {count}");
+        break;
+    }  
+}
+
